a9b0f3d155fc697322e099ef69ef57021db46415
emeneces <eumenecesz@gmail.com>
2025-12-14 23:06:28 -0400

    test: add test integration with postman collection

--- src/test/java/edu/nur/nurtricenter/mealplans/application/ingredient/CreateIngredientHandlerTest.java
+++ src/test/java/edu/nur/nurtricenter/mealplans/application/ingredient/CreateIngredientHandlerTest.java
@@ -2,6 +2,7 @@ package edu.nur.nurtricenter.mealplans.application.ingredient;
 
 import an.awesome.pipelinr.Pipeline;
 import edu.nur.nurtricenter.mealplans.domain.shared.UnitMeasureEnum;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ class CreateIngredientHandlerTest {
 
     private final Pipeline pipeline;
 
+    @Inject
     public CreateIngredientHandlerTest(Pipeline pipeline) {
         this.pipeline = pipeline;
     }
